7a1a59d6a29810712189f09066fb2eed05147241
Silumesii Maboshe <silumesii@pencilcasestudios.com>
2015-09-17 13:05:48 +0200

    Updated high_voltage configuration.

--- config/initializers/high_voltage.rb
+++ config/initializers/high_voltage.rb
@@ -1,5 +1,7 @@
-# Ref: https://github.com/thoughtbot/high_voltage#specifying-a-root-path
 # config/initializers/high_voltage.rb
+# Ref: https://github.com/thoughtbot/high_voltage#specifying-a-root-path
+# Ref: https://github.com/thoughtbot/high_voltage#top-level-routes
 HighVoltage.configure do |config|
 	config.home_page = 'home'
+	config.route_drawer = HighVoltage::RouteDrawers::Root
 end
